30e6990c52afecad6602b24e0425e23c70271852
Angie Yuliana Pedraza Barrera <angiey_pedrazaq@soy.sena.edu.co>
2026-07-02 21:37:16 -0500

    holiii

--- sena/sena/settings.py
+++ sena/sena/settings.py
@@ -66,16 +66,9 @@ WSGI_APPLICATION = 'sena.wsgi.application'
 # ========== BASE DE DATOS MySQL ==========
 DATABASES = {
     'default': {
-        'ENGINE':   'django.db.backends.mysql',
+        'ENGINE':   'django.db.backends.sqlite3',
         'NAME':     os.environ.get('DB_NAME', 'proyectoo'),
-        'USER':     os.environ.get('DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', '1234567890'),
-        'HOST':     os.environ.get('DB_HOST', 'localhost'),
-        'PORT':     os.environ.get('DB_PORT', '3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        
     }
 }
 
